7dd2166de5b0d4d9d2a3075a6ea090a95b76ee38
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-08-13 18:12:26 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/patrickmendes/workshopmongodb/services/UserService.java
@@ -0,0 +1,20 @@
+package com.patrickmendes.workshopmongodb.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.patrickmendes.workshopmongodb.domain.User;
+import com.patrickmendes.workshopmongodb.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
