e5fa2b1620aa8ed0b9f798f0d9f4451d7158d7fa
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-08-13 22:00:34 -0300

    Obtendo um post por id

---
+++ src/main/java/com/patrickmendes/workshopmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.patrickmendes.workshopmongodb.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.patrickmendes.workshopmongodb.domain.Post;
+import com.patrickmendes.workshopmongodb.repository.PostRepository;
+import com.patrickmendes.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Post user = repo.findById(id).orElse(null);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+}
